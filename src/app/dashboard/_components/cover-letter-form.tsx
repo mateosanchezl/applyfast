54515ab00bfab3ef4447c0f9a8d26b2a4e848d7d
mateo <mateosanchez.msl@gmail.com>
2024-10-25 16:08:01 +0100

    first commit

---
+++ src/app/dashboard/_components/cover-letter-form.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+function CoverLetterForm() {
+  return <div>CoverLetterForm</div>;
+}
+
+export default CoverLetterForm;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CoverLetterForm from "./_components/cover-letter-form";
+
+function DashboardPage() {
+  return (
+    <div className="h-screen">
+      <CoverLetterForm />
+    </div>
+  );
+}
+
+export default DashboardPage;
