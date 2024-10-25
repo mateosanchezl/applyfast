1a4d19601a0a52f19b176f8d3a653bc056e371b0
mateo <mateosanchez.msl@gmail.com>
2024-10-25 21:19:14 +0100

    pdf clean + validation

--- src/app/api/generate/helpers.ts
+++ src/app/api/generate/helpers.ts
@@ -1,5 +1,7 @@
 import PDFParser from "pdf2json";
 
+export const MAX_FILE_SIZE = 1 * 1024 * 1024;
+
 export const parsePdf = async (pdfFile: Blob): Promise<string> => {
   const arrayBuffer = await pdfFile.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
@@ -23,4 +25,11 @@ export const parsePdf = async (pdfFile: Blob): Promise<string> => {
 
     parser.parseBuffer(buffer);
   });
+};
+
+export const removeExcessWhitespace = (text: string) => {
+  text = text.trim();
+  // Replace multiple whitespaces with one
+  text = text.replace(/\s+/g, " ");
+  return text;
 };
--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import PDFParser from "pdf2json";
-import { parsePdf } from "./helpers";
+import { MAX_FILE_SIZE, parsePdf, removeExcessWhitespace } from "./helpers";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const formData = await req.formData();
@@ -12,6 +11,20 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     return NextResponse.json({ Error: "No job_url or file." }, { status: 400 });
   }
 
-  const text = await parsePdf(file);
+  if (file.type !== "application/pdf") {
+    return NextResponse.json({ Error: "Invalid file type." }, { status: 400 });
+  }
+
+  if (file.size > MAX_FILE_SIZE) {
+    return NextResponse.json(
+      { Error: `File size exceeds the limit of ${MAX_FILE_SIZE / (1024 * 1024)} MB.` },
+      { status: 400 }
+    );
+  }
+
+  // Parse and clean pdf
+  let text = await parsePdf(file);
+  text = removeExcessWhitespace(text);
+
   return NextResponse.json({ text: text, job_url: job_url }, { status: 201 });
 }
